b915d797ef20d6cf8b02a6910a177cbd8e48c092
RamieleLetsch <ramid.letsch@gmail.com>
2026-06-02 15:20:18 -0300

    Add files via upload

---
+++ aula 24.04.2026/imprime5/imprime5/imprime5_010.py
@@ -0,0 +1,23 @@
+
+"""
+Programa: imprime5
+Descrição:Este programa imprime na tela os cinco primeiros números naturais. Esta versão elimina a alocação desnecessária de memória
+Autor: Ramiele D. Letsch
+Data: 24/03/2026
+Versão: 0.1.0
+"""
+
+# Alocação de memória:
+
+i = 0
+
+
+# Entrada de dados
+
+# Processamento de dados
+
+# Saída de dados
+
+while i < 5:
+    print(i+1)
+    i=i+1
